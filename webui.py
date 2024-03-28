0fe1c55ed83615cae58a421f7f3ec565f2604ccc
HitCongWang <2149367891@qq.com>
2024-03-28 17:12:17 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -17,18 +17,53 @@ if __name__ == "__main__":
     is_lite = "lite" in sys.argv
 
     st.set_page_config(
-        "CampusLinker WebUI",
-        os.path.join("img", "CampusLinker.jpg"),
+        "智慧科研辅助平台 WebUI",
+        os.path.join("img", "zhihuikeyanjiaoxue.png"),
         initial_sidebar_state="expanded",
         # page_bg_color="white",
         # theme
         menu_items={
             'Get Help': 'https://github.com/chatchat-space/Langchain-Chatchat',
             'Report a bug': "https://github.com/chatchat-space/Langchain-Chatchat/issues",
-            'About': f"""欢迎使用 CampusLinker WebUI {VERSION}！"""
+            'About': f"""欢迎使用 智慧科研辅助平台 WebUI {VERSION}！"""
         }
         
     )
+    # st.set_page_config(
+    #     "CampusLinker WebUI",
+    #     os.path.join("img", "CampusLinker.jpg"),
+    #     initial_sidebar_state="expanded",
+    #     # page_bg_color="white",
+    #     # theme
+    #     menu_items={
+    #         'Get Help': 'https://github.com/chatchat-space/Langchain-Chatchat',
+    #         'Report a bug': "https://github.com/chatchat-space/Langchain-Chatchat/issues",
+    #         'About': f"""欢迎使用 CampusLinker WebUI {VERSION}！"""
+    #     }
+        
+    # )
+
+    # st.markdown(
+    #     """
+    #     <style>
+    #     .css-1v4eu6t {
+    #         background-color: #ffffff; /* 将 #ffffff 替换为您喜欢的白色色值 */
+    #     }
+    #     </style>
+    #     """,
+    #     unsafe_allow_html=True
+    # )
+
+    # st.set_page_config(
+    #     "Langchain-Chatchat WebUI",
+    #     os.path.join("img", "CampusLinker.png"),
+    #     initial_sidebar_state="expanded",
+    #     menu_items={
+    #         'Get Help': 'https://github.com/chatchat-space/Langchain-Chatchat',
+    #         'Report a bug': "https://github.com/chatchat-space/Langchain-Chatchat/issues",
+    #         'About': f"""欢迎使用 CampusLinker WebUI {VERSION}！"""
+    #     }
+    # )
 
     st.markdown(
         """
@@ -54,14 +89,14 @@ if __name__ == "__main__":
             st.image(
                 os.path.join(
                     "img",
-                    "CampusLinker.jpg"
-                ),
-                use_column_width=True
+                    "zhihuikeyanjiaoxue.png",
+                    use_column_width=True
             )
-            # st.caption(
-            #     f"""<p align="right">当前版本：{VERSION}</p>""",
-            #     unsafe_allow_html=True,
-            # )
+            )
+            st.caption(
+                f"""<p align="right">当前版本：{VERSION}</p>""",
+                unsafe_allow_html=True,
+            )
             options = list(pages)
             icons = [x["icon"] for x in pages.values()]
 
@@ -73,13 +108,13 @@ if __name__ == "__main__":
                 # menu_icon="chat-quote",
                 default_index=default_index,
             )
-            file_uploader = st.file_uploader("选择本地文件",type=["csv","txt","xlsx"])
+            file_uploader = st.file_uploader("选择本地文件",type=["csv","txt","xlsx","pdf"])
             if file_uploader is not None:
                 local_path = "/home/root1/wcc/Langchain-Chatchat/save_excel"
                 os.makedirs(local_path,exist_ok=True)
                 filename = file_uploader.name
                 
-                dialogue.NAME_EXCEL = filename
+                dialogue.NAME_FILE = filename
                 with open(os.path.join(local_path, filename),"wb") as f:
                     f.write(file_uploader.read())
                 st.success(f"文件成功保存到 {local_path}/{filename} ")
